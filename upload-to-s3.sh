43e7af9d9257f8d948d135fe7211b90b6e44dfe3
Kyungyong Lee <leeky@kookmin.ac.kr>
2016-08-10 06:46:17 +0000

    narrow down regions

--- upload-to-s3.sh
+++ upload-to-s3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 hn=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-
-#for bn in "spot-instance-tf-checkpoint-eu-west-1" "spot-instance-tf-checkpoint-us-east-1" "spot-instance-tf-checkpoint-us-west-1" "spot-instance-tf-checkpoint-us-west-2" "spot-instance-tf-checkpoint-eu-central-1" "spot-instance-tf-checkpoint-ap-ne-1"
-for bn in "spot-instance-tf-checkpoint-eu-west-1" "spot-instance-tf-checkpoint-us-west-1" "spot-instance-tf-checkpoint-us-west-2" "spot-instance-tf-checkpoint-eu-central-1" "spot-instance-tf-checkpoint-ap-ne-1"
-
+for i in {1..10}
 do
-  stime=$(($(date +%s%N)/1000000))
-  aws s3 cp /run/shm/6700000-simple-ckpt-model.ckpt-0 s3://"$bn"/ --region eu-west-1
-  etime=$(($(date +%s%N)/1000000))
-  let "duration = $etime - $stime"
-  echo -e "$hn\t$bn\t$duration"
+  for bn in "spot-instance-tf-checkpoint-eu-west-1" "spot-instance-tf-checkpoint-us-east-1" "spot-instance-tf-checkpoint-us-west-1" "spot-instance-tf-checkpoint-ap-ne-1"
+  do
+    stime=$(($(date +%s%N)/1000000))
+    aws s3 cp /run/shm/53600000-simple-ckpt-model.ckpt-0 s3://"$bn"/"$hn" --region eu-west-1 1>&2
+    etime=$(($(date +%s%N)/1000000))
+    let "duration = $etime - $stime"
+    echo -e "$hn\t$bn\t$duration"
+  done
 done
 
